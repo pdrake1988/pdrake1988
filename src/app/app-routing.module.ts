ea7f159c44ef773888f4427d507d481509f1b8f8
pdrake1988 <pdrake1@me.com>
2021-01-25 11:09:39 -0600

    Added Movie Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './Movie Project/movies/movies.component';
-import {AppComponent} from './app.component';
 
 const routes: Routes = [
   {path: '/Movies', component: MoviesComponent}
