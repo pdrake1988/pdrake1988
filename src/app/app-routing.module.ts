8f2f1b7fb292e73f4014f4e2994edb6fcc4afb4e
pdrake1988 <pdrake1@me.com>
2021-01-25 18:50:25 -0600

    Added Home component and some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './Movie Project/movies/movies.component';
+import {HomeComponent} from './Home/home/home.component';
 
 const routes: Routes = [
-  {path: '/Movies', component: MoviesComponent}
+  {path: 'Movies', component: MoviesComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './Movie Project/movies/movies.component';
+import { HomeComponent } from './Home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
